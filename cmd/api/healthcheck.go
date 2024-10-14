f6d2669a02d2cf456a7e536830140f6b93497dbf
Jesse Neal <jesse.neal@jesse.neal-CXQX3JNHFD>
2024-10-14 08:07:04 -0500

    adding dependencies

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,7 +8,5 @@ import (
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-    fmt.Fprintf(w, "version: %s\n", version)
-
-    
+	fmt.Fprintf(w, "version: %s\n", version)
 }
